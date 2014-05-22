467da84f77625d370a26de51d4188eddaae6d3fb
Sebastian Bartos <seth.kriticos@gmail.com>
2014-05-22 16:07:02 +0200

    integrated FileBrowser in MainWindow
    ripped out the old junk
    
    note: git control buttons put on hold until
          command widget is properly implemented

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -35,17 +35,8 @@ public:
 
 private slots:
     void updatePresets();
-    void setRootPath();
-    void setPath(QString path);
+
     void setCommand(QString cmd, QString path);
-    void on_config_presets_clicked();
-    void charmClicked(QString path);
-    void on_wpb_home_clicked();
-    void on_wb_folders_clicked(const QModelIndex &index);
-    void on_wpb_terminal_clicked();
-    void on_wpb_folder_clicked();
-    void on_view_file_clicked();
-    void on_edit_file_clicked();
 
     void on_werb_exec_clicked();
     void on_wer_cmd_returnPressed();
@@ -53,30 +44,14 @@ private slots:
     void onMyStderrReadReady();
     void onMyProcessFinished(int exitCode);
 
-    void on_actionScmPull_triggered();
-    void on_actionScmCommit_triggered();
-    void on_actionScmPush_triggered();
-
-    void onFileSelected(QItemSelection selected);
-    void wfInitBrowser();
-
     void on_btnClearCommand_clicked();
-
     void on_wer_cmd_textEdited(const QString &arg1);
 
 private:
-    FileBrowser *fb;
-
     Ui::MainWindow *ui;
-    QString path;
-    QList<QPushButton> charmButtons;
     QShortcut *shortEsc;
-    QFileSystemModel *dirmodel, *filemodel;
-    ConfigWidget *wconfig;
     DataStore *ds;
 
-    QList<CharmButton*> cblist;
-    QList<QLabel*> cllist;
     QProcess *guiCmdProcess;
     bool processRunnning;
     bool isInit;
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -25,58 +25,44 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent), ui(new Ui::MainWindow
 
     // variables
     this->processRunnning = false;
-    this->filemodel = NULL;
 
     // keyboard shortcuts
     this->shortEsc = new QShortcut(QKeySequence(tr("Esc")), this);
     connect(shortEsc, SIGNAL(activated()), this, SLOT(close()));
 
-    this->wfInitBrowser();
-
     // file and command quicklists
     ui->wfileinner->init(ds);
-    connect(ui->wfileinner, SIGNAL(forwardPath(QString)), this, SLOT(setPath(QString)));
+    // TODO: add signal to filelist that sends selected file path to connect with filebrowser
     connect(this->ui->wfileinner, SIGNAL(openOrEditClicked()), this, SLOT(close()));
 
     ui->wcmdinner->init(ds);
     connect(ui->wcmdinner, SIGNAL(commandSelected(QString,QString)),
             this, SLOT(setCommand(QString,QString)));
 
-    // configuration widget (preferences)
-    this->wconfig = new ConfigWidget(ds, this);
-
     // presets
-    this->connect(wconfig, SIGNAL(accepted()), this, SLOT(updatePresets()));
     this->updatePresets();
 
-    connect(ui->wprb0, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb1, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb2, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb3, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb4, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb5, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb6, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb7, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb8, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
-    connect(ui->wprb9, SIGNAL(presetClicked(QString)), this, SLOT(setPath(QString)));
+    connect(ui->wprb0, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb1, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb2, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb3, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb4, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb5, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb6, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb7, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb8, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
+    connect(ui->wprb9, SIGNAL(presetClicked(QString)), ui->fileBrowser, SLOT(setSelectedFolder(QString)));
 
     this->ui->notesWidget->initWidget(this->ds);
+    this->ui->fileBrowser->initFileBrowser(this->ds);
 
-    if(ds->tblGeneral->contains("selected_file")){
-        QString filePath = ds->getGeneralValue("selected_file");
-        if(!filePath.isEmpty()){
-            ui->wb_files->setCurrentIndex(this->filemodel->index(filePath));
-        }
-    }
-    if(ds->tblGeneral->contains("current_command")){
+    connect(ui->fileBrowser, SIGNAL(fileSelected(QString)),
+            ui->notesWidget, SLOT  (selectedFileChanged(QString)));
+
+    if(ds->tblGeneral->contains("current_command"))
         ui->wer_cmd->setText(ds->getGeneralValue("current_command"));
-    }
 
     this->isInit = false;
-
-    this->fb = new FileBrowser();
-    this->fb->initFileBrowser(this->ds);
-    this->fb->show();
 }
 
 /**
@@ -109,7 +95,7 @@ void MainWindow::exec(QStringList cmd)
     connect(process, SIGNAL(finished(int)), this, SLOT(onMyProcessFinished(int)));
     this->processRunnning = true;
     this->ui->wer_cmd->clear();
-    process->setWorkingDirectory(this->path);
+    process->setWorkingDirectory(ui->fileBrowser->getSelectedFolder());
 
     this->ui->wcmdinner->update();
 
@@ -138,7 +124,7 @@ void MainWindow::multiExec(QList<QStringList> commands)
         QProcess *proc = new QProcess(this);
         QString cmd = commands.at(i).at(0);
         QStringList args = commands.at(i); args.removeFirst();
-        proc->setWorkingDirectory(this->path);
+        proc->setWorkingDirectory(ui->fileBrowser->getSelectedFolder());
         proc->start(cmd, args);
         proc->waitForFinished(60000);
         this->ui->we_output->append(proc->readAll());
@@ -173,166 +159,10 @@ void MainWindow::updatePresets()
     ui->wprb9->setText(ds->getPreset(9));
 }
 
-void MainWindow::setRootPath()
-{
-    this->setPath(QDir::rootPath());
-}
-
-void MainWindow::setPath(QString path)
-{
-    this->path = path;
-    ds->setGeneralValue("navigator_path", path);
-    if(!this->isInit){
-        ds->setGeneralValue("selected_file", "");
-        ui->notesWidget->selectedFileChanged("");
-    }
-
-    // split path into directories
-    QStringList charmParts = path.split(QDir::separator());
-
-    ui->wb_folders->setCurrentIndex(dirmodel->index(path));
-    ui->wb_folders->setExpanded(dirmodel->index(path), true);
-    ui->wb_files->setRootIndex(filemodel->setRootPath(path));
-
-    // clear previous buttons and labels
-    for (int i=cblist.size()-1; i>=0; i--){
-        ui->horizontalLayout_2->removeWidget(cblist.at(i));
-        delete this->cblist.at(i);
-        cblist.removeAt(i);
-    }
-    for (int i=cllist.size()-1; i>=0; i--){
-        ui->horizontalLayout_2->removeWidget(cllist.at(i));
-        delete this->cllist.at(i);
-        cllist.removeAt(i);
-    }
-
-    // add charm buttons to charm navigation
-    if (path != "/") {
-        for (int i=1; i<charmParts.size(); i++){
-            QString p;
-            for (int j=1; j<=i; j++)
-                p += "/" + charmParts.at(j);
-            CharmButton *b = new CharmButton(charmParts.at(i), p, ui->wpcharms);
-            this->cblist.append(b);
-            connect(b, SIGNAL(charmClicked(QString)), this, SLOT(setPath(QString)));
-
-            int k = ui->horizontalLayout_2->count();
-            ui->horizontalLayout_2->insertWidget(k-1, b);
-            if (i < charmParts.size()-1){
-                QLabel *l = new QLabel("/", ui->wpcharms);
-                this->cllist.append(l);
-                ui->horizontalLayout_2->insertWidget(k, l);
-            }
-        }
-    }
-}
-
 void MainWindow::setCommand(QString cmd, QString path)
 {
-    this->setPath(path);
+    ui->fileBrowser->setSelectedFolder(path);
     this->ui->wer_cmd->setText(cmd);
-}
-
-void MainWindow::on_config_presets_clicked()
-{
-    wconfig->show();
-}
-
-void MainWindow::charmClicked(QString path)
-{
-    this->setPath(path);
-}
-
-void MainWindow::on_wpb_home_clicked()
-{
-   setPath(QDir::homePath());
-}
-
-void MainWindow::on_wb_folders_clicked(const QModelIndex &index)
-{
-   QString path = dirmodel->fileInfo(index).absoluteFilePath();
-   this->setPath(path);
-}
-
-void MainWindow::on_wpb_terminal_clicked()
-{
-    QProcess *p = new QProcess(this);
-    QStringList sl;
-    sl = ds->getGeneralValue("terminal_emulator").split(" ");
-    QString cmd = sl.at(0);
-    sl.removeAt(0);
-    sl << this->path;
-    p->start(cmd, QStringList() << sl);
-
-    this->close();
-}
-
-void MainWindow::on_wpb_folder_clicked()
-{
-    QProcess *p = new QProcess(this);
-    QStringList sl;
-    sl = ds->getGeneralValue("file_browser").split(" ");
-    QString cmd = sl.at(0);
-    sl.removeAt(0);
-    sl << this->path;
-    p->start(cmd, QStringList() << sl);
-
-    this->close();
-}
-
-void MainWindow::on_view_file_clicked()
-{
-    // get currently selected file
-    QModelIndex index = ui->wb_files->selectionModel()->selectedIndexes().at(0);
-    QString selectedFilePath = filemodel->fileInfo(index).absoluteFilePath();
-
-    // extract filetype
-    QString extStr = selectedFilePath.split(".").last();
-
-    DsTable::Record record = ds->tblExtensions->getRecord(extStr);
-    QString extActPri = record.value("ext_act_pri").toString();
-
-    // prepare command string and arguments
-    QStringList rawCmd = extActPri.split(" ");
-    QString prog = rawCmd.first();
-    QStringList args = rawCmd.mid(1) << selectedFilePath;
-
-    // execute open command
-    QProcess *p = new QProcess(this);
-    p->startDetached(prog, args);
-
-    // push file to stack
-    ds->setFile(selectedFilePath);
-    ui->wfileinner->update();
-
-    this->close();
-}
-
-void MainWindow::on_edit_file_clicked()
-{
-    // get currently selected file
-    QModelIndex index = ui->wb_files->selectionModel()->selectedIndexes().at(0);
-    QString selectedFilePath = filemodel->fileInfo(index).absoluteFilePath();
-
-    // extract filetype
-    QString extStr = selectedFilePath.split(".").last();
-
-    // prepare command string and arguments
-    DsTable::Record record = ds->tblExtensions->getRecord(extStr);
-    QString extActSec = record.value("ext_act_sec").toString();
-
-    QString prog = extActSec.split(" ").at(0);
-    QStringList args = QStringList() << extActSec.split(" ").mid(1) << selectedFilePath;
-
-    // execute open command
-    QProcess *p = new QProcess(this);
-    p->startDetached(prog, args);
-
-    // push file to stack
-    ds->setFile(selectedFilePath);
-    ui->wfileinner->update();
-
-    this->close();
 }
 
 // Execute command, read output and populate output window
@@ -361,9 +191,9 @@ void MainWindow::on_werb_exec_clicked()
     connect(process, SIGNAL(finished(int)), this, SLOT(onMyProcessFinished(int)));
     this->processRunnning = true;
     this->ui->we_output->append(raw);
-    process->setWorkingDirectory(this->path);
+    process->setWorkingDirectory(ui->fileBrowser->getSelectedFolder());
 
-    ds->setCommand(raw, this->path);
+    ds->setCommand(raw, ui->fileBrowser->getSelectedFolder());
     this->ui->wcmdinner->update();
 
     process->startDetached(prog, args);
@@ -406,80 +236,6 @@ void MainWindow::onMyProcessFinished(int exitCode)
 void MainWindow::on_wer_cmd_returnPressed()
 {
     this->on_werb_exec_clicked();
-}
-
-/**
- * @brief Perform SCM pull action at current path
- */
-void MainWindow::on_actionScmPull_triggered()
-{
-    this->exec(QStringList() << "/usr/bin/git" << "pull");
-}
-
-/**
- * @brief Perform SCM add all + commit action
- */
-void MainWindow::on_actionScmCommit_triggered()
-{
-    bool ok;
-    QString text = QInputDialog::getText(this, tr("Commit message"),
-                                         tr("Commit message:"), QLineEdit::Normal, "", &ok);
-    if (ok){
-        if (!text.isEmpty()){
-            QList <QStringList> commands;
-            commands.append(QStringList() << "/usr/bin/git" << "add" << "--all");
-            commands.append(QStringList() << "/usr/bin/git" << "commit" << "-am" << text);
-            this->multiExec(commands);
-        } else {
-            QMessageBox::warning(this, "Abortig commit", "No commit message, aborting..", QMessageBox::Ok);
-        }
-    }
-}
-
-/**
- * @brief Perform SCM push
- */
-void MainWindow::on_actionScmPush_triggered()
-{
-    this->exec(QStringList() << "/usr/bin/git" << "push" << "all" << "--all");
-}
-
-void MainWindow::onFileSelected(QItemSelection selected)
-{
-    /*
-    QModelIndex index = selected.indexes().first();
-    QString selectedFilePath = dirmodel->fileInfo(index).absoluteFilePath();
-    ds->setGeneralValue("selected_file", selectedFilePath);
-
-    ui->notesWidget->selectedFileChanged(selectedFilePath);
-    */
-}
-
-void MainWindow::wfInitBrowser()
-{
-    this->dirmodel = new QFileSystemModel(this);
-    this->filemodel = new QFileSystemModel(this);
-
-    this->dirmodel->setFilter(QDir::NoDotAndDotDot | QDir::Dirs);
-    this->filemodel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
-
-    ui->wb_folders->setModel(dirmodel);
-    ui->wb_files->setModel(filemodel);
-
-    ui->wb_folders->setRootIndex(dirmodel->setRootPath("/"));
-    ui->wb_folders->hideColumn(3);
-    ui->wb_folders->hideColumn(2);
-    ui->wb_folders->hideColumn(1);
-
-    if(ds->tblGeneral->contains("navigator_path"))
-        this->setPath(ds->getGeneralValue("navigator_path"));
-    else
-        this->setPath(QDir::homePath());
-
-    QItemSelectionModel *fileSelectionModel = ui->wb_files->selectionModel();
-    connect(ui->wpc_root, SIGNAL(clicked()), this, SLOT(setRootPath()));
-    connect(fileSelectionModel, SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
-            this, SLOT(onFileSelected(QItemSelection)));
 }
 
 void MainWindow::on_btnClearCommand_clicked()
